723d6226282892597a624721f9736dec4041a314
donovan2121 <gilchrist068@gmail.com>
2022-09-05 17:59:13 +0800

    refactor dirname and file extension to use dict type

--- downloads_sorter.py
+++ downloads_sorter.py
@@ -1,15 +1,16 @@
 import os, shutil
 from pathlib import Path
 
-default_dir = ('images', 'videos', 'audios', 'documents')
-image = ('png', 'jpg', 'gif', 'webp')
-video = ('mp4', 'mpeg')
-audio = ('mp3', 'flac')
-document = ('pdf', 'docx', 'xlsx', 'csv', 'txt')
-found_files = []
+dir_file_dict = {'images': ('png', 'jpg', 'gif', 'webp'), 'videos': ('mp4', 'mpeg'), 
+'audios': ('mp3', 'flac'), 'documents': ('pdf', 'docx', 'xlsx', 'csv', 'txt')}
+# images = ('png', 'jpg', 'gif', 'webp')
+# videos = ('mp4', 'mpeg')
+# audios = ('mp3', 'flac')
+# documents = ('pdf', 'docx', 'xlsx', 'csv', 'txt')
+found_files = {}
 
 def check_dir():
-    for dir in default_dir:
+    for dir in dir_file_dict.keys():
         path_dir = Path.cwd() / dir
         if path_dir.exists() and path_dir.is_dir():
             print(f'The {dir} exists.')
@@ -18,14 +19,18 @@ def check_dir():
             print(f'The {dir} has been created.')
 
 def find_files(path):
-    for file_type in (image, video, audio, document):
+    for dir_name, file_type in dir_file_dict.items():
         for ext in file_type:
             file_list = list(path.glob(f'*.{ext}*'))
             if len(file_list):
-                found_files.append(file_list)
+                # create a dict type with keys image,video,audio,
+                found_files.setdefault(path / dir_name, file_list)
 
-# def move_files():
-#     shutil.move()
+# def move_files(found_files):
+#     if len(found_files):
+#         for file_type in found_files:
+#             for file in file_type:
+#                 shutil.move()
 
 check_dir()
 find_files(Path.cwd())
